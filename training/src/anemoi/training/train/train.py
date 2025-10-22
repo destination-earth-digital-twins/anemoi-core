f1c46ba23a97b2efb183faf970e92d251ad28b02
arams <arams@met.no>
2025-10-22 15:35:05 +0200

    minor fixes in train.py

--- training/src/anemoi/training/train/train.py
+++ training/src/anemoi/training/train/train.py
@@ -59,19 +59,18 @@ class AnemoiTrainer:
         # This can increase performance (and TensorCore usage, where available).
         torch.set_float32_matmul_precision("high")
         # Resolve the config to avoid shenanigans with lazy loading
-        # for now... for debugging
-        # if config.config_validation:
-        #     OmegaConf.resolve(config)
-        #     self.config = BaseSchema(**config)
 
-        #     LOGGER.info("Config validated.")
-        # else:
-        #     config = OmegaConf.to_object(config)
-        #     self.config = UnvalidatedBaseSchema(**DictConfig(config))
+        if config.config_validation:
+            OmegaConf.resolve(config)
+            self.config = BaseSchema(**config)
 
-        #     LOGGER.info("Skipping config validation.")
-        OmegaConf.resolve(config)
-        self.config = config
+            LOGGER.info("Config validated.")
+        else:
+            config = OmegaConf.to_object(config)
+            self.config = UnvalidatedBaseSchema(**DictConfig(config))
+
+            LOGGER.info("Skipping config validation.")
+
         # Choose between single domain and multi-domain runs
         self.dynamic_mode = self.config.model.dynamic_mode
         assert isinstance(
